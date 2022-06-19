9cae87c08fbc3cfe077f606939c5567b4f0054a0
Curious-Coder-2001 <mr.liar.2001@gmail.com>
2022-06-19 12:34:27 +0500

    1st chapter done

---
+++ lib/contact.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+class Contact {
+  final String name;
+  final String id;
+  Contact({required this.name}) : id = const Uuid().v4();
+}
+
+class ContactBook extends ValueNotifier<List<Contact>> {
+  ContactBook._sharedInstance() : super([]);
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() {
+    return _shared;
+  }
+
+  int get length => value.length;
+
+  void add({required Contact contact}) {
+    final contacts = value;
+    contacts.add(contact);
+    notifyListeners();
+  }
+
+  void remove({required Contact contact}) {
+    final contacts = value;
+    if (contacts.contains(contact)) {
+      contacts.remove(contact);
+      notifyListeners();
+    }
+  }
+
+  Contact? contact({required int atIndex}) {
+    return value.length > atIndex ? value[atIndex] : null;
+  }
+}
