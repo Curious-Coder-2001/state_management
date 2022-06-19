f1e2a2dc4563ddc21a6736d70e4ff360e02222b3
Curious-Coder-2001 <mr.liar.2001@gmail.com>
2022-06-19 23:53:55 +0500

    chapter 2 done

--- lib/chapter 2/main.dart
+++ lib/chapter 2/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -6,7 +7,10 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: HomePage(),
+    home: ApiProvider(
+      api: Api(),
+      child: const HomePage(),
+    ),
   ));
 }
 
@@ -18,23 +22,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String title = 'Welcome back sir!';
+  ValueKey textKey = const ValueKey<String?>(null);
 
   @override
   Widget build(BuildContext context) {
+    final api = ApiProvider.of(context).api;
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(api.dateAndTime ?? ''),
       ),
       body: InkWell(
-        onTap: () {
+        onTap: () async {
+          final dateAndTime = await api.getDateAndTime();
           setState(() {
-            title = 'Thanks man!';
+            textKey = ValueKey(dateAndTime);
           });
         },
-        child: Container(
-          color: Colors.blueGrey[900],
+        child: SizedBox.expand(
+          child: Container(
+            color: Colors.blueGrey[900],
+            child: DateTimeWidget(key: textKey),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class Api {
+  String? dateAndTime;
+
+  Future<String> getDateAndTime() {
+    return Future.delayed(
+      const Duration(seconds: 1),
+      () => DateTime.now().toIso8601String(),
+    ).then((value) {
+      dateAndTime = value;
+      return value;
+    });
+  }
+}
+
+class ApiProvider extends InheritedWidget {
+  final Api api;
+  final String uuid;
+
+  ApiProvider({Key? key, required this.api, required Widget child})
+      : uuid = const Uuid().v4(),
+        super(
+          key: key,
+          child: child,
+        );
+
+  @override
+  bool updateShouldNotify(covariant ApiProvider oldWidget) {
+    return uuid != oldWidget.uuid;
+  }
+
+  static ApiProvider of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ApiProvider>()!;
+  }
+}
+
+class DateTimeWidget extends StatelessWidget {
+  const DateTimeWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        ApiProvider.of(context).api.dateAndTime ?? "Tap on the screen to see magic!)))",
       ),
     );
   }
